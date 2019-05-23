2d1c472e1efcbf2db61781f08ac47389b9732c05
Arqam Bin Rehan <arqambinrehan@gmail.com>
2019-05-23 12:00:27 +0500

    first commit

---
+++ test/MessageList.test.js
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils'
+import MessageList from '../src/components/MessageList'
+
+describe('MessageList.test.js', () => {
+  let cmp
+
+  beforeEach(() => {
+    cmp = mount(MessageList, {
+      // Be aware that props is overridden using `propsData`
+      propsData: {
+        messages: ['Cat']
+      }
+    })
+  })
+
+  it('has received ["Cat"] as the message property', () => {
+    expect(cmp.vm.messages).toEqual(['Cat'])
+  })
+
+  it('contains 1 li element, with', () => {
+    expect(cmp.findAll('li').length).toBe(1)
+    expect(cmp.findAll('li').at(0).text()).toMatch('Cat')
+  })
+
+  it('has the expected html structure', () => {
+    expect(cmp.element).toMatchSnapshot()
+  })
+})
